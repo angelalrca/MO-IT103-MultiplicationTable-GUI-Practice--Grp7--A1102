4443ae0769f890b9188ff35c6b9e521c0dbeabc0
angelalrca <161995976+angelalrca@users.noreply.github.com>
2024-05-28 08:32:16 +0800

    Update MultiplicationTable.java

--- src/MultiplicationTable.java
+++ src/MultiplicationTable.java
@@ -1,27 +1,135 @@
-public class MultiplicationTable {
+import javax.swing.JOptionPane;
 
-    public static void main(String[] args) {
-        int[][] multiplicationTable = generateMultiplicationTable(20);
-        printMultiplicationTable(multiplicationTable);
+public class MultiTab extends javax.swing.JFrame {
+
+    public MultiTab() {
+        initComponents();
     }
 
-    public static int[][] generateMultiplicationTable(int size) {
-        int[][] table = new int[size][size];
-        for(int i = 0; i < size; i++) {
-            for(int j = 0; j < size; j++) {
-                table[i][j] = (i + 1) * (j + 1);
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        jLabel1 = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        txtshowtable = new javax.swing.JTextPane();
+        jLabel2 = new javax.swing.JLabel();
+        btnMulti = new javax.swing.JButton();
+        btnReset = new javax.swing.JButton();
+        txttablenum = new javax.swing.JTextField();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("", 1, 18)); 
+        jLabel1.setText("Multiplication Table");
+
+        jScrollPane1.setViewportView(txtshowtable);
+
+        jLabel2.setFont(new java.awt.Font("Segoe UI", 1, 14)); 
+        jLabel2.setText("Enter the table number:");
+
+        btnMulti.setText("Multiply");
+        btnMulti.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnMultiActionPerformed(evt);
             }
-        }
-        return table;
+        });
+
+        btnReset.setText("Reset");
+        btnReset.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnResetActionPerformed(evt);
+            }
+        });
+
+        txttablenum.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txttablenumActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(101, 101, 101)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jLabel2)
+                        .addGap(18, 18, 18)
+                        .addComponent(txttablenum, javax.swing.GroupLayout.PREFERRED_SIZE, 71, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(15, 15, 15)
+                        .addComponent(btnMulti)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(btnReset))
+                    .addGroup(layout.createSequentialGroup()
+                        .addContainerGap()
+                        .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 283, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(99, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addComponent(jLabel1)
+                .addGap(11, 11, 11)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel2)
+                    .addComponent(txttablenum, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 149, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(31, 31, 31)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(btnReset)
+                    .addComponent(btnMulti, javax.swing.GroupLayout.PREFERRED_SIZE, 25, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(23, 23, 23))
+        );
+
+        pack();
     }
 
-    public static void printMultiplicationTable(int[][] table) {
-        for(int i = 0; i < table.length; i++) {
-            for(int j = 0; j < table[i].length; j++) {
-                System.out.printf("%5d", table[i][j]);
+    private void btnMultiActionPerformed(java.awt.event.ActionEvent evt) {
+         try {
+            int num = Integer.parseInt(txttablenum.getText());
+            int limit = 1;
+            txtshowtable.setText("");
+            while (limit <= 10) {
+                txtshowtable.setText(txtshowtable.getText() + num + " x " + limit + " = " + (num * limit) + "\n");
+                limit++;
             }
-            System.out.println();
+        } catch (Exception e) {
+            txttablenum.setText("");
+            JOptionPane.showMessageDialog(null, "Enter a valid Number", "Just Number", JOptionPane.INFORMATION_MESSAGE);
+            txttablenum.requestFocus();
         }
     }
-    
+
+    private void txttablenumActionPerformed(java.awt.event.ActionEvent evt) {
+        btnMultiActionPerformed(evt);
+    }
+
+    private void btnResetActionPerformed(java.awt.event.ActionEvent evt) {
+        txttablenum.setText("");
+        txtshowtable.setText("");
+    }
+    public static void main(String args[]) {
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new MultiTab().setVisible(true);
+            }
+        });
+    }
+
+    private javax.swing.JButton btnMulti;
+    private javax.swing.JButton btnReset;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTextPane txtshowtable;
+    private javax.swing.JTextField txttablenum;
+
 }
